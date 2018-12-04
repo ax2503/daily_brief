155aee27cef37836544de973deb696c64054c382
Garry Saunders <ax2503@gmail.com>
2018-12-04 12:22:15 +1000

    Now finds goldOZ

--- example.py
+++ example.py
@@ -6,7 +6,11 @@ import requests
 def main():
   url = 'https://www.ainsliebullion.com.au/'
   r=requests.get(url, allow_redirects = True)
-  open('ainslie.html', 'wb').write(r.content) 
-   
+  open('ainslie.html', 'wb').write(r.content)
+  f = open('ainslie.html','r')
+  text = f.read()
+  match = re.search(r'goldOZ = \d+', text)
+  if match : print (match.group()) 
+    
 if __name__ == '__main__':
   main()
