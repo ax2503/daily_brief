f8e7da407d10bb3107005c2f0a3c67922b24214b
ax2503 <ax2503@gmail.com>
2018-12-04 15:02:02 +1000

    calculation into a function

--- example.py
+++ example.py
@@ -1,9 +1,7 @@
 import re
 import requests
 
-
-
-def main():
+def calcPMprices():
   url = 'https://www.ainsliebullion.com.au/'
   r=requests.get(url, allow_redirects = True)
   open('ainslie.html', 'wb').write(r.content)
@@ -21,9 +19,30 @@ def main():
   else :
     aud = 0
 
+  match = re.search(r'(silverOZ = )(\d+\.\d+)',text)
+  if match :
+    silverOZ = float(match.group(2))
+  else :
+    silverOZ = 0
+
   audgold = goldOZ/aud
   audgold = ((int)(audgold * 100))/100
-  print('gold oz in aud = ' + str(audgold))
+  audsilver = silverOZ/aud
+  audsilver = ((int)(audsilver * 100))/100
+  return [audgold, audsilver]  
+
+
+
+def main():
+  PMprices = calcPMprices()
+  gold = PMprices[0]
+  silver =PMprices[1]
+
+
+  print('gold oz in aud = ' + str(gold))
+  print('silver oz in aud = ' + str(silver))
+  stash = (int)((gold * 10 + silver * 689)*100)/100
+  print('value of stash = ' + str(stash))
 
 if __name__ == '__main__':
   main()
