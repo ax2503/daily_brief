4282a132c98ff2ec86acfe69d470ae931606e407
Garry Saunders <ax2503@gmail.com>
2018-12-07 12:47:14 +1000

    gets all story titles

--- news.py
+++ news.py
@@ -1,12 +1,16 @@
 #Program to import a list of headlines from News Sources
 import requests
+import re
 
 def main() :
   url = 'https://www.newsnow.co.uk/h/'
   r=requests.get(url, allow_redirects = True)
   open('newsnow.html', 'wb').write(r.content)
   f = open('newsnow.html','r')
-
+  text = f.read()
+  match = re.findall(r'nofollow\">[^<]+',text)
+  for k in match :
+    print(k[10:])
 
 
   f.close()
