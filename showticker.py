d7645d1097dde980253030b003795e943c3a3b03
ax2503 <ax2503@gmail.com>
2018-12-07 10:38:56 +1000

    with new program

---
+++ showticker.py
@@ -0,0 +1,18 @@
+#This program takes a ticker symbol and displays all the date in stocks.fs for the keys that match
+import sys
+import pickledb
+
+
+
+def main() :
+  db = pickledb.load('stocks.fs', True)
+  args = sys.argv[1:]
+
+  if not args :
+    for k in db.getall() :
+      print(k[:10] + ' ' + k[10:] + ' ' + db.get(k))
+
+  db.dump()  
+
+if __name__ == '__main__':
+  main()
