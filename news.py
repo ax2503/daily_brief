7b20a2f6f43b55165765b85f6f2a6b747f261df6
ax2503 <ax2503@gmail.com>
2018-12-07 13:30:31 +1000

    allows one search term

--- news.py
+++ news.py
@@ -1,17 +1,24 @@
 #Program to import a list of headlines from News Sources
 import requests
 import re
+import sys
+
 
 def main() :
+  args = sys.argv[1:]
   url = 'https://www.newsnow.co.uk/h/'
   r=requests.get(url, allow_redirects = True)
   open('newsnow.html', 'wb').write(r.content)
   f = open('newsnow.html','r')
   text = f.read()
   match = re.findall(r'nofollow\">[^<]+',text)
-  for k in match :
-    print(k[10:])
-
+  if not args :
+    for k in match :
+      print(k[10:])
+  else :
+    for k in match :
+      if args[0] in k :
+        print(k[10:])
 
   f.close()
 
