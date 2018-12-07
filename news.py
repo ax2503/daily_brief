65be584b8a13aa8bcd269bd728198ebeabe33b81
ax2503 <ax2503@gmail.com>
2018-12-07 14:33:35 +1000

    multiple args, added Google News

--- news.py
+++ news.py
@@ -3,25 +3,58 @@ import requests
 import re
 import sys
 
-
-def main() :
-  args = sys.argv[1:]
-  url = 'https://www.newsnow.co.uk/h/'
+def GoogleNews(args) :
+  url = 'https://news.google.com/?hl=en-AU&gl=AU&ceid=AU:en'
   r=requests.get(url, allow_redirects = True)
-  open('newsnow.html', 'wb').write(r.content)
-  f = open('newsnow.html','r')
-  text = f.read()
-  match = re.findall(r'nofollow\">[^<]+',text)
+  open('googlenews.html','wb').write(r.content)
+  f=open('googlenews.html','r')
+  try :
+    text = f.read()
+  except UnicodeDecodeError :
+    text = ''
+  f.close()
+  match = re.findall(r'<span>([^<]+)',text)
+  print("**********Google News**********")
   if not args :
     for k in match :
-      print(k[10:])
+      print(k)
   else :
     for term in args :
       for k in match :
         if term in k :
-          print(k[10:])
+          print(k)
+  return
 
+def NewsNow(args) :
+  url = 'https://www.newsnow.co.uk/h/'
+  r=requests.get(url, allow_redirects = True)
+  open('newsnow.html', 'wb').write(r.content)
+  f = open('newsnow.html','r')
+  try :
+    text = f.read()
+  except UnicodeDecodeError :
+    text = ''
   f.close()
+  match = re.findall(r'nofollow\">([^<]+)',text)
+  print('**********News Now**********')
+  if not args :
+    for k in match :
+      print(k)
+  else :
+    for term in args :
+      for k in match :
+        if term in k :
+          print(k)
+  return  
+
+
+def main() :
+  args = sys.argv[1:]
+  GoogleNews(args)
+  NewsNow(args)
+
+
+
 
 
 if __name__ == '__main__':
